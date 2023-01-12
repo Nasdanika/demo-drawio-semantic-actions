9b489fb45e22770d75acf761599c5a526a6dd18a
Pavel <Pavel.Vlasov@nasdanika.org>
2023-01-12 18:50:05 -0500

    Refinement.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,5 @@
 module org.nasdanika.demo.drawio.semanticmapping {
 		
-	requires org.eclipse.emf.ecore.xmi;
-	requires org.nasdanika.html.emf;
 	requires org.nasdanika.html.model.app.gen;
-	requires sitemapgen4j;
 	
 }
